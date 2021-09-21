d5541811364e69fb5cec472ee83f78a66a29f38a
SvetlanaFoglia <luce62006@gmail.com>
2021-09-21 14:20:34 +0200

    fix

---
+++ src/Feeds.widget.js
@@ -0,0 +1,44 @@
+export class FeedsWidget {
+    constructor(selector, config) {
+
+        // получаем контейнер (куда надо разместить себя)
+        const elContainer = document.querySelector(selector);
+        if (!elContainer) {
+            console.error('Контейнер не найден');
+            return;
+        }
+
+        const html = `
+            <h2 className="card-title h4">Фиды</h2>
+            <ul className="list-group border-0 rounded-0">            
+            </ul>
+            `;
+
+        elContainer.insertAdjacentHTML('afterbegin', html);
+
+        this.uiElements = {
+            elFeeds: document.querySelector(`${selector} ul`)
+        }
+    }
+
+    // добавить feed
+    addFeed(feed) {
+        const li = document.createElement('li');
+        li.classList.add('mb-3');
+
+        const h4 = document.createElement('h4');
+        li.append(h4);
+
+        const a = document.createElement('a');
+        a.classList.add('text-decoration-none');
+        a.textContent = feed.title;
+        h4.append(a);
+
+        const p = document.createElement('p');
+        p.textContent = feed.description;
+        li.append(p);
+
+        this.uiElements.elFeeds.insertAdjacentElement('beforeend', li);
+    }
+
+}
