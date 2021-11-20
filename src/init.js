27ade92fcbedd4403f52568740f457cc0e395ebd
SvetlanaFoglia <luce62006@gmail.com>
2021-11-20 22:58:04 +0100

    fix

--- src/init.js
+++ src/init.js
@@ -1,127 +1,124 @@
-import i18next from "i18next";
+import i18next from 'i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { renderLanguage, watchState } from './view.js';
 import _ from 'lodash';
-import axios from "axios";
-import parse from "./parse.js";
- import resources from "./locales/index.js"
-import * as yup from "yup";
+import axios from 'axios';
+import * as yup from 'yup';
+import { renderLanguage, watchState } from './view.js';
+import parse from './parse.js';
+import resources from './locales/index.js';
 
 export default () => {
-    const elements = {
-        input: document.querySelector('#input-form'),
-        form: document.querySelector('.rss-form'),
-        feedback: document.querySelector('.feedback'),
-        button: document.querySelector('[name="add"]'),
-        modal: document.querySelector('#modal'),
-        feeds: document.querySelector('#feeds'),
-        posts: document.querySelector('#posts'),
-        readLink: document.querySelector('#readLink'),
-        modalTitle: document.querySelector('.modal-title'),
-        modalBody: document.querySelector('.modal-body'),
-        modalClose: document.querySelector('#close'),
-        closeButton: document.querySelector('#close-button'),
-    };
-console.log(elements.input)
-    const schema = yup.string('NotString').url('notUrl').required('notRequired');
+  const elements = {
+    input: document.querySelector('#input-form'),
+    form: document.querySelector('.rss-form'),
+    feedback: document.querySelector('.feedback'),
+    button: document.querySelector('[name="add"]'),
+    modal: document.querySelector('#modal'),
+    feeds: document.querySelector('#feeds'),
+    posts: document.querySelector('#posts'),
+    readLink: document.querySelector('#readLink'),
+    modalTitle: document.querySelector('.modal-title'),
+    modalBody: document.querySelector('.modal-body'),
+    modalClose: document.querySelector('#close'),
+    closeButton: document.querySelector('#close-button'),
+  };
+  console.log(elements.input);
+  const schema = yup.string('NotString').url('notUrl').required('notRequired');
 
-    i18next.init({
-        lng: 'ru',
-        debug: true,
-        resources,
-    }).then(()=>{
-
+  i18next.init({
+    lng: 'ru',
+    debug: true,
+    resources,
+  }).then(() => {
     const state = {
-        loadingProcess: {
-            status: 'idle',
-            error: null,
+      loadingProcess: {
+        status: 'idle',
+        error: null,
+      },
+      feeds: [],
+      posts: [],
+      form: {
+        processState: 'filling',
+        fields: {
+          url: '',
         },
-        feeds: [],
-        posts: [],
-        form: {
-            processState: 'filling',
-            fields: {
-                url: '',
-            },
-            valid: null,
-            error: null,
-        },
-        ui: {
-            postId: null,
-            seenPosts: new Set(),
-        },
+        valid: null,
+        error: null,
+      },
+      ui: {
+        postId: null,
+        seenPosts: new Set(),
+      },
     };
 
     const watchedState = watchState(state, elements);
     const getUrlWithProxy = (url) => {
-        const newUrl = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
-        newUrl.searchParams.set('disableCache', 'true');
-        newUrl.searchParams.set('url', url);
-        return newUrl.toString();
+      const newUrl = new URL('/get', 'https://hexlet-allorigins.herokuapp.com');
+      newUrl.searchParams.set('disableCache', 'true');
+      newUrl.searchParams.set('url', url);
+      return newUrl.toString();
     };
 
     const validate = (url) => {
-        const urls = watchedState.feeds.map((feed) => feed.url);
-        const newSchema = schema.notOneOf(urls, 'theSame');
-        try {
-            newSchema.validateSync(url);
-            watchedState.form.valid = true;
-            watchedState.form.error = null;
-        } catch (e) {
-            watchedState.form.valid = false;
-            watchedState.form.error = e.message;
-        }
+      const urls = watchedState.feeds.map((feed) => feed.url);
+      const newSchema = schema.notOneOf(urls, 'theSame');
+      try {
+        newSchema.validateSync(url);
+        watchedState.form.valid = true;
+        watchedState.form.error = null;
+      } catch (e) {
+        watchedState.form.valid = false;
+        watchedState.form.error = e.message;
+      }
     };
 
     const handleInput = (e) => {
-        console.log(e)
-        watchedState.form.processState = 'filling';
-        watchedState.form.fields.url = e.target.value;
-        validate(watchedState.form.fields.url);
-
-
+      console.log(e);
+      watchedState.form.processState = 'filling';
+      watchedState.form.fields.url = e.target.value;
+      validate(watchedState.form.fields.url);
     };
 
     const getErrorType = (error) => {
-        if (error.isAxiosError) {
-            return 'networkError';
-        } if (error.isParsingError) {
-            return 'parsingError';
-        }
-        return 'unknownError';
+      if (error.isAxiosError) {
+        return 'networkError';
+      } if (error.isParsingError) {
+        return 'parsingError';
+      }
+      return 'unknownError';
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(e);
-        watchedState.loadingProcess.status = 'sending';
-        if (watchedState.form.valid === false) {
-            watchedState.loadingProcess.status = 'formError';
-            return;
-        }
-        const url = getUrlWithProxy(state.form.fields.url);
+      e.preventDefault();
+      console.log(e);
+      watchedState.loadingProcess.status = 'sending';
+      if (watchedState.form.valid === false) {
+        watchedState.loadingProcess.status = 'formError';
+        return;
+      }
+      const url = getUrlWithProxy(state.form.fields.url);
 
-        axios.get(url)
-            .then((response) => {
-                const data = parse(response.data.contents);
-                const feed = {
-                    description: data.description,
-                    url: state.form.fields.url,
-                    title: data.title,
-                    id: _.uniqueId(),
-                };
-                const posts = data.items.map((item) => ({ id: _.uniqueId(), ...item, feedId: feed.id }));
-                watchedState.feeds.unshift(feed);
-                watchedState.posts.unshift(...posts);
-                watchedState.loadingProcess.status = 'success';
-                watchedState.form.error = null;
-            })
-            .catch((error) => {
-                state.loadingProcess.error = getErrorType(error);
-                watchedState.loadingProcess.status = 'loadingError';
-                watchedState.form.error = null;
-            });
+      axios.get(url)
+        .then((response) => {
+          const data = parse(response.data.contents);
+          const feed = {
+            description: data.description,
+            url: state.form.fields.url,
+            title: data.title,
+            id: _.uniqueId(),
+          };
+          const posts = data.items.map((item) => ({ id: _.uniqueId(), ...item, feedId: feed.id }));
+          watchedState.feeds.unshift(feed);
+          watchedState.posts.unshift(...posts);
+          watchedState.loadingProcess.status = 'success';
+          watchedState.form.error = null;
+        })
+        .catch((error) => {
+          state.loadingProcess.error = getErrorType(error);
+          watchedState.loadingProcess.status = 'loadingError';
+          watchedState.form.error = null;
+        });
     };
 
     renderLanguage();
@@ -129,29 +126,29 @@ console.log(elements.input)
 
     elements.form.addEventListener('submit', handleSubmit);
 
-
     const refreshTime = 5000;
 
     const refreshFeeds = () => {
-        const promises = watchedState.feeds.map((feed) => axios.get(getUrlWithProxy(feed.url))
-            .then((response) => {
-                const parsedRss = parse(response.data.contents);
-                const result = _.differenceWith(parsedRss.items,
-                    watchedState.posts, (a, b) => a.title === b.title);
-                const posts = result.map((item) => ({ id: _.uniqueId(), ...item, feedId: feed.id }));
-                watchedState.posts.unshift(...posts);
-            }).catch(_.noop));
-        Promise.all(promises)
-            .finally(() => {
-                setTimeout(refreshFeeds, refreshTime);
-            });
+      const promises = watchedState.feeds.map((feed) => axios.get(getUrlWithProxy(feed.url))
+        .then((response) => {
+          const parsedRss = parse(response.data.contents);
+          const result = _.differenceWith(parsedRss.items,
+            watchedState.posts, (a, b) => a.title === b.title);
+          const posts = result.map((item) => ({ id: _.uniqueId(), ...item, feedId: feed.id }));
+          watchedState.posts.unshift(...posts);
+        }).catch(_.noop));
+      Promise.all(promises)
+        .finally(() => {
+          setTimeout(refreshFeeds, refreshTime);
+        });
     };
     setTimeout(refreshFeeds, refreshTime);
     elements.posts.addEventListener('click', (e) => {
-        if (!('id' in e.target.dataset)) {
-            return;
-        }
-        watchedState.ui.postId = e.target.dataset.id;
-        watchedState.ui.seenPosts.add(e.target.dataset.id);
+      if (!('id' in e.target.dataset)) {
+        return;
+      }
+      watchedState.ui.postId = e.target.dataset.id;
+      watchedState.ui.seenPosts.add(e.target.dataset.id);
     });
-})}
+  });
+};
--- src/view.js
+++ src/view.js
@@ -1,138 +1,137 @@
-import onChange from "on-change";
+import onChange from 'on-change';
 import i18next from 'i18next';
 
 const renderLanguage = () => {
-    document.querySelectorAll('[data-i18n]').forEach((el) => {
-        el.textContent = i18next.t(`${el.dataset.i18n}`);
-    });
+  document.querySelectorAll('[data-i18n]').forEach((el) => {
+    el.textContent = i18next.t(`${el.dataset.i18n}`);
+  });
 };
 
 const watchState = (state, elements) => {
-    const renderSuccessText = () => {
-        elements.input.classList.remove('is-invalid');
-        elements.feedback.textContent = i18next.t('success');
-        elements.feedback.classList.remove('text-danger');
-        elements.feedback.classList.remove('text-info');
-        elements.feedback.classList.add('text-success');
-        elements.input.value = '';
-        elements.input.focus();
-        elements.button.removeAttribute('disabled');
-        elements.input.removeAttribute('readonly');
-    };
+  const renderSuccessText = () => {
+    elements.input.classList.remove('is-invalid');
+    elements.feedback.textContent = i18next.t('success');
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-info');
+    elements.feedback.classList.add('text-success');
+    elements.input.value = '';
+    elements.input.focus();
+    elements.button.removeAttribute('disabled');
+    elements.input.removeAttribute('readonly');
+  };
 
-    const renderSendingData = () => {
-        elements.input.setAttribute('readonly', true);
-        elements.button.setAttribute('disabled', true);
-        elements.feedback.classList.remove('text-danger');
-        elements.feedback.classList.remove('text-success');
-        elements.feedback.classList.add('text-info');
-        elements.feedback.textContent = i18next.t('loading');
-    };
+  const renderSendingData = () => {
+    elements.input.setAttribute('readonly', true);
+    elements.button.setAttribute('disabled', true);
+    elements.feedback.classList.remove('text-danger');
+    elements.feedback.classList.remove('text-success');
+    elements.feedback.classList.add('text-info');
+    elements.feedback.textContent = i18next.t('loading');
+  };
 
-    const renderFeeds = (stateValue) => {
-        const feedsContent = stateValue.feeds.map((feed) => {
-            const title = `<h3>${feed.title}</h3>`;
-            const description = `<p>${feed.description}</p>`;
-            const titleline= `<li class="list-group-item">${title}${description}</li>`;
-            return titleline;
-        }).join('');
-        const htmlFeeds = `<h3>${i18next.t('feeds')}</h3><ul>${feedsContent}</ul>`;
-        elements.feeds.innerHTML = `${htmlFeeds}`;
-    };
+  const renderFeeds = (stateValue) => {
+    const feedsContent = stateValue.feeds.map((feed) => {
+      const title = `<h3>${feed.title}</h3>`;
+      const description = `<p>${feed.description}</p>`;
+      const titleline = `<li class="list-group-item">${title}${description}</li>`;
+      return titleline;
+    }).join('');
+    const htmlFeeds = `<h3>${i18next.t('feeds')}</h3><ul>${feedsContent}</ul>`;
+    elements.feeds.innerHTML = `${htmlFeeds}`;
+  };
 
-    const getPostsHtml = ({ posts, ui: { seenPosts } }) => {
-        const listPosts = posts.map(({id, title, link,}) => {
-            const className = seenPosts.has(id) ? 'fw-normal link-secondary' : 'fw-bold';
-            const resultButtons= `<li class="list-group-item d-flex justify-content-between align-itemsstart">
+  const getPostsHtml = ({ posts, ui: { seenPosts } }) => {
+    const listPosts = posts.map(({ id, title, link }) => {
+      const className = seenPosts.has(id) ? 'fw-normal link-secondary' : 'fw-bold';
+      const resultButtons = `<li class="list-group-item d-flex justify-content-between align-itemsstart">
     <a href="${link}" class="${className}" data-id="${id}" data-id-text="${id}" target="_blank">${title}</a>
     <button id = "details" data-bs-toggle="modal" data-bs-target="#modal" 
     class="btn btn-primary btn-sm pull-right" data-id=${id}>${i18next.t('watch')}</button></span></li>`;
-            return resultButtons;
-        }).join('');
+      return resultButtons;
+    }).join('');
 
-        return `<ul>${listPosts}</ul>`;
-    };
+    return `<ul>${listPosts}</ul>`;
+  };
 
-    const renderPosts = (stateValue) => {
-        elements.posts.innerHTML = `<h3>${i18next.t('posts')}</h3>${getPostsHtml(stateValue)}`;
-    };
+  const renderPosts = (stateValue) => {
+    elements.posts.innerHTML = `<h3>${i18next.t('posts')}</h3>${getPostsHtml(stateValue)}`;
+  };
 
-    const renderModal = (stateValue) => {
-        const {
-            id, title, link, description,
-        } = stateValue.posts
-            .find((post) => post.id === stateValue.ui.postId);
-        const selectedText = document.querySelector(`[data-id-text="${id}"]`);
-        elements.readLink.setAttribute('href', `${link}`);
-        selectedText.classList.remove('font-weight-bold');
-        elements.modalTitle.textContent = `${title}`;
-        elements.modalBody.textContent = `${description}`;
-    };
+  const renderModal = (stateValue) => {
+    const {
+      id, title, link, description,
+    } = stateValue.posts
+      .find((post) => post.id === stateValue.ui.postId);
+    const selectedText = document.querySelector(`[data-id-text="${id}"]`);
+    elements.readLink.setAttribute('href', `${link}`);
+    selectedText.classList.remove('font-weight-bold');
+    elements.modalTitle.textContent = `${title}`;
+    elements.modalBody.textContent = `${description}`;
+  };
 
-    const renderFormError = (stateValue) => {
-        elements.input.classList.add('is-invalid', true);
-        elements.feedback.classList.add('text-danger');
-        elements.feedback.textContent = i18next.t(`${stateValue.form.error}`);
-        elements.input.removeAttribute('readonly');
-        elements.button.removeAttribute('disabled');
-    };
+  const renderFormError = (stateValue) => {
+    elements.input.classList.add('is-invalid', true);
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = i18next.t(`${stateValue.form.error}`);
+    elements.input.removeAttribute('readonly');
+    elements.button.removeAttribute('disabled');
+  };
 
-    const renderLoadingError = (stateValue) => {
-        elements.input.classList.add('is-invalid', true);
-        elements.feedback.classList.add('text-danger');
-        elements.feedback.textContent = i18next.t(`${stateValue.loadingProcess.error}`);
-        elements.input.removeAttribute('readonly');
-        elements.button.removeAttribute('disabled');
-    };
+  const renderLoadingError = (stateValue) => {
+    elements.input.classList.add('is-invalid', true);
+    elements.feedback.classList.add('text-danger');
+    elements.feedback.textContent = i18next.t(`${stateValue.loadingProcess.error}`);
+    elements.input.removeAttribute('readonly');
+    elements.button.removeAttribute('disabled');
+  };
 
-    const router = (value, stateValue) => {
-        switch (value) {
-            case 'sending':
-                renderSendingData();
-                break;
-            case 'success':
-                renderSuccessText();
-                break;
-            case 'formError':
-                renderFormError(stateValue);
-                break;
-            case 'loadingError':
-                renderLoadingError(stateValue);
-                break;
-            default:
-                break;
-        }
-    };
+  const router = (value, stateValue) => {
+    switch (value) {
+      case 'sending':
+        renderSendingData();
+        break;
+      case 'success':
+        renderSuccessText();
+        break;
+      case 'formError':
+        renderFormError(stateValue);
+        break;
+      case 'loadingError':
+        renderLoadingError(stateValue);
+        break;
+      default:
+        break;
+    }
+  };
 
-    const watchedState = onChange(state, (path, value) => {
-        switch (path) {
-            case 'loadingProcess.status':
-                router(value, state);
-                break;
-            case 'feeds': {
-                renderFeeds(state);
-                break;
-            }
-            case 'posts': {
-                renderPosts(state);
-                break;
-            }
-            case 'ui.postId': {
-                renderModal(state);
-                break;
-            }
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'loadingProcess.status':
+        router(value, state);
+        break;
+      case 'feeds': {
+        renderFeeds(state);
+        break;
+      }
+      case 'posts': {
+        renderPosts(state);
+        break;
+      }
+      case 'ui.postId': {
+        renderModal(state);
+        break;
+      }
 
-            case 'ui.seenPosts': {
-                renderPosts(state);
-                break;
-            }
+      case 'ui.seenPosts': {
+        renderPosts(state);
+        break;
+      }
 
-            default:
-                break;
-        }
-    });
-    return watchedState;
+      default:
+        break;
+    }
+  });
+  return watchedState;
 };
 
 export { renderLanguage, watchState };
-
